5dfb8ab0d7841870c9d0eb263db3cf7b382a908f
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-08-10 21:53:18 -0300

    refactar: create main

--- aula01/index.py
+++ aula01/index.py
@@ -1,5 +1,5 @@
 import pandas as pd
-def main ():
+def aula01 ():
     df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
     #df is a data frame
 
@@ -84,5 +84,57 @@ def main ():
     print(df.head())
 
     print(df.describe(include="object"))
+    return df
 
-main()
+def dadosAula01():
+    df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
+    # Dicionário de renomeação
+    novos_nomes = {
+        'work_year': 'ano',
+        'experience_level': 'senioridade',
+        'employment_type': 'contrato',
+        'job_title': 'cargo',
+        'salary': 'salario',
+        'salary_currency': 'moeda',
+        'salary_in_usd': 'usd',
+        'employee_residence': 'residencia',
+        'remote_ratio': 'remoto',
+        'company_location': 'empresa',
+        'company_size': 'tamanho_empresa'
+    }
+
+    # Aplicando renomeação
+    df.rename(columns=novos_nomes, inplace=True)
+
+    senioridade = {
+        'SE': 'senior',
+        'MI': 'pleno',
+        'EN': 'junior',
+        'EX': 'executivo'
+    }
+    df['senioridade'] = df['senioridade'].replace(senioridade)
+
+    contrato = {
+        'FT': 'integral',
+        'PT': 'parcial',
+        'CT': 'contrato',
+        'FL': 'freelancer'
+    }
+    df['contrato'] = df['contrato'].replace(contrato)
+
+    tamanho_empresa = {
+        'L': 'grande',
+        'S': 'pequena',
+        'M':	'media'
+
+    }
+    df['tamanho_empresa'] = df['tamanho_empresa'].replace(tamanho_empresa)
+
+    mapa_trabalho = {
+        0: 'presencial',
+        100: 'remoto',
+        50: 'hibrido'
+    }
+
+    df['remoto'] = df['remoto'].replace(mapa_trabalho)
+    return df
---
+++ index.py
@@ -0,0 +1,4 @@
+from aula01.index import dadosAula01
+df=dadosAula01()
+
+print(df.head())
